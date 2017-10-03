a1d08dd0f0346e1985986b42b0569f3c8bf2a982
aploftus <aploftus@users.noreply.github.com>
2017-10-03 09:53:37 -0700

    Implement exponent for non-neg nums, need to fix neg nums

--- src/recursion.js
+++ src/recursion.js
@@ -65,6 +65,20 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+
+	if (exp === 0) {
+		return 1;
+	}
+	if (exp === 1) {
+		return base;
+	}
+	if (exp % 2 === 0) {
+		return exponent(base, exp / 2) ** 2;
+	}
+	if (exp < 0) {
+		return exponent(base, exp + 1) / base;
+	}
+	return base * exponent(base, exp - 1);
 };
 
 // 8. Determine if a number is a power of two.
